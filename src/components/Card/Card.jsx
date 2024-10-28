150acb98b7fe0f2c403881751a5f40753f40ada6
JBB <jbaptistebesancon@gmail.com>
2024-10-28 18:43:30 +0100

    Début màj header + carte projet

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,16 @@ import Tag from '../Tag/Tag';
 function Card({ title, description, tags, repo }) {
   return (
     <div className="card">
-      <h3>{title}</h3>
-      <div className="card__tags">
-        {tags.map((tag, index) => {
-          return <Tag title={tag} key={index} />;
-        })}
+      <div className="card__title">
+        <h3>{title}</h3>
+        <div className="card__tags">
+          {tags.map((tag, index) => {
+            return <Tag title={tag} key={index} />;
+          })}
+        </div>
+      </div>
+      <div className="card__description">
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,26 @@ import './Header.scss';
 
 function Header() {
   return (
-    <header className="header">
+    <header id="header" className="header">
       <h1>Jean-Baptiste Besançon</h1>
       <nav>
         <ul>
           <li>
-            <a href="profil">Profil</a>
-          </li>
-          <li>
-            <a href="projects">Portfolio</a>
-          </li>
-          <li>
-            <a href="skills">Skills</a>
-          </li>
-          <li>
-            <a href="contact">Contact</a>
+            <a href="header">Home</a>
+            <ul className="sous">
+              <li>
+                <a href="profil">Profil</a>
+              </li>
+              <li>
+                <a href="projects">Portfolio</a>
+              </li>
+              <li>
+                <a href="skills">Skills</a>
+              </li>
+              <li>
+                <a href="contact">Contact</a>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
